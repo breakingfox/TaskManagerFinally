fb6ed7342f0791f92a242466ad515b2491331a99
Atamyrat <atashkhozhaev20000@gmail.com>
2020-05-21 13:40:06 +0300

    SplashActivity

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_OUT_TIME = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_OUT_TIME);
+    }
+}
